bfc29fa4abfaf40017c68ed425461a81bbf1bea0
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 20:40:37 +0530

    chore: add test for sameDigitNumber function

---
+++ sameDigitNumber.test.js
@@ -0,0 +1,16 @@
+const file = require("./sameDigitNumbers");
+
+const isSameDigitNumberA = file.isSameDigitNumberA;
+const isSameDigitNumberN = file.isSameDigitNumberN;
+
+test("Testing sameDigitNumber normal function", () => {
+  expect(isSameDigitNumberN(222)).toBe(true);
+  expect(isSameDigitNumberN(232)).toBe(false);
+  expect(isSameDigitNumberN(999999999)).toBe(true);
+});
+
+test("Testing sameDigitNumber arrow function", () => {
+  expect(isSameDigitNumberA(00000)).toBe(true);
+  expect(isSameDigitNumberA(22)).toBe(true);
+  expect(isSameDigitNumberA(211111112)).toBe(false);
+});
