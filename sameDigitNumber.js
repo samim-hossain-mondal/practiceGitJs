8c2b6b2b3cf33972c398b0c6608f81bd0b7bbd7d
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 16:21:27 +0530

    feat: add functionality to check all digits are same in number using arrow function

--- sameDigitNumber.js
+++ sameDigitNumber.js
@@ -1,11 +1,20 @@
 const readline = require("readline-sync");
-
 let number = Number(readline.question());
-let result = 0;
-function isSameDigitNumber(num) {
-  result = Number(String(num).split("").reverse().join(""));
+
+let resultN = 0;
+function isSameDigitNumberN(num) {
+  resultN = Number(String(num).split("").reverse().join(""));
   return result === num
     ? "True: It is a same digit number"
     : "False: It is not a same digit number";
 }
-console.log(isSameDigitNumber(number));
+
+let resultA = 0;
+let isSameDigitNumberA = (num) => {
+  resultA = Number(String(num).split("").reverse().join(""));
+  return result === num
+    ? "True: It is a same digit number"
+    : "False: It is not a same digit number";
+};
+console.log(isSameDigitNumberN(number));
+console.log(isSameDigitNumberA(number));
