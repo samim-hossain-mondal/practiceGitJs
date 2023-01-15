971e32e12dda41e6fb199604cf6ff2e901531b09
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 16:30:15 +0530

    feat: add functionality to count number of vowels in string using arrow function

--- countVowels.js
+++ countVowels.js
@@ -2,7 +2,7 @@ const readline = require("readline-sync");
 let inputString = String(readline.question());
 
 const vowels = ["a", "e", "i", "o", "u"];
-function countVowel(str) {
+function countVowelN(str) {
   let count = 0;
   str = str.toLowerCase();
   function isVowel(item) {
@@ -12,4 +12,15 @@ function countVowel(str) {
   return count;
 }
 
-console.log(countVowel(inputString));
+let countVowelA = (str) => {
+  let count = 0;
+  str = str.toLowerCase();
+  function isVowel(item) {
+    count += vowels.includes(item) ? 1 : 0;
+  }
+  str.split("").forEach(isVowel);
+  return count;
+};
+
+console.log(countVowelN(inputString));
+console.log(countVowelA(inputString));
