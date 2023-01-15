4f5fbec79d0784ac18e05127a5e6edb5c2cb26a2
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 22:21:29 +0530

    feat: add functionality to concat two strings without first letter

--- concateStrings.js
+++ concateStrings.js
@@ -1,15 +1,23 @@
 const readline = require("readline-sync");
 
+// User Inputs
 let inputString1 = String(readline.question());
 let inputString2 = String(readline.question());
 
+// Normal Function
 function concateStringsWithoutFirstLetterN(str1, str2) {
   return str1.substr(1, str1.length) + str2.substr(1, str2.length);
 }
+console.log(concateStringsWithoutFirstLetterN(inputString1, inputString2));
 
+// Arrow Function
 let concateStringsWithoutFirstLetterA = (str1, str2) => {
   return str1.substr(1, str1.length) + str2.substr(1, str2.length);
 };
-
-console.log(concateStringsWithoutFirstLetterN(inputString1, inputString2));
 console.log(concateStringsWithoutFirstLetterA(inputString1, inputString2));
+
+// Exporting Functions
+module.exports = {
+  concateStringsWithoutFirstLetterN,
+  concateStringsWithoutFirstLetterA,
+};
