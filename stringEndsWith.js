2e1d88d82b206e7b22a0d9dafffdceed3cfb7872
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 20:28:14 +0530

    feat: add functionality to check string ends with 'Script'

--- stringEndsWith.js
+++ stringEndsWith.js
@@ -1,15 +1,21 @@
 const readline = require("readline-sync");
+
+// User Input
 let inputString = String(readline.question());
 
+// Normal Function
 function checkEndsWithN(str) {
   let checkStr = "Script";
   return str.endsWith(checkStr);
 }
+console.log(checkEndsWithN(inputString));
 
+// Arrow Function
 let checkEndsWithA = (str) => {
   let checkStr = "Script";
   return str.endsWith(checkStr);
 };
-
-console.log(checkEndsWithN(inputString));
 console.log(checkEndsWithA(inputString));
+
+// Exporting Functions
+module.exports = { checkEndsWithN, checkEndsWithA };
