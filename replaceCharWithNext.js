87f33d8dc70f883b88b21dab1cd01183fd94e4c8
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 16:23:53 +0530

    feat: add functionality to replace current with next character in string using arrow function

--- replaceCharWithNext.js
+++ replaceCharWithNext.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync");
 let inputString = String(readline.question());
 
-function replaceWithNextChar(str) {
+function replaceWithNextCharN(str) {
   let charArray = str.split("");
   for (let item = 0; item < charArray.length; item++) {
     let n = charArray[item].charCodeAt() - "a".charCodeAt();
@@ -11,4 +11,15 @@ function replaceWithNextChar(str) {
   return charArray.join("");
 }
 
-console.log(replaceWithNextChar(inputString));
+let replaceWithNextCharA = (str) => {
+  let charArray = str.split("");
+  for (let item = 0; item < charArray.length; item++) {
+    let n = charArray[item].charCodeAt() - "a".charCodeAt();
+    n = (n + 1) % 26;
+    charArray[item] = String.fromCharCode(n + "a".charCodeAt());
+  }
+  return charArray.join("");
+};
+
+console.log(replaceWithNextCharN(inputString));
+console.log(replaceWithNextCharA(inputString));
