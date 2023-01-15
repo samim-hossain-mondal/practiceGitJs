b884bbf75f6d1f77462837b4a2826c631bd0a30e
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 22:37:10 +0530

    feat: add functionality to check year is leap

--- checkLeapYear.js
+++ checkLeapYear.js
@@ -1,17 +1,23 @@
 const readline = require("readline-sync");
+
+// User Input
 let year = Number(readline.question());
 
+// Normal Function
 function checkLeapYearN(year) {
   return (0 == year % 4 && 0 != year % 100) || 0 == year % 400
     ? `${year} is a leap year"`
     : `${year} is not a leap year`;
 }
+console.log(checkLeapYearN(year));
 
+// Arrow Function
 let checkLeapYearA = (year) => {
   return (0 == year % 4 && 0 != year % 100) || 0 == year % 400
     ? `${year} is a leap year"`
     : `${year} is not a leap year`;
 };
-
-console.log(checkLeapYearN(year));
 console.log(checkLeapYearA(year));
+
+// Exporting Functions
+module.exports = { checkLeapYearN, checkLeapYearA };
