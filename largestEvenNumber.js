ab969156a0429680cdb679a3c20e1b110a26e783
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 16:28:12 +0530

    feat: add functionality to find largest even number in array using arrow function

--- largestEvenNumber.js
+++ largestEvenNumber.js
@@ -6,7 +6,7 @@ for (let i = 0; i < arrSize; ++i) {
   arr.push(Number(readline.question()));
 }
 
-function largestEvenNumberNormal(arr) {
+function largestEvenNumberA(arr) {
   let result = Number.MIN_SAFE_INTEGER;
   function findLargestEven(item) {
     result = item % 2 == 0 && item > result ? item : result;
@@ -15,4 +15,14 @@ function largestEvenNumberNormal(arr) {
   return (result = result != Number.MIN_SAFE_INTEGER ? result : -1);
 }
 
-console.log(largestEvenNumberNormal(arr));
+let largestEvenNumberA = (arr) => {
+  let result = Number.MIN_SAFE_INTEGER;
+  function findLargestEven(item) {
+    result = item % 2 == 0 && item > result ? item : result;
+  }
+  arr.forEach(findLargestEven);
+  return (result = result != Number.MIN_SAFE_INTEGER ? result : -1);
+};
+
+console.log(largestEvenNumberN(arr));
+console.log(largestEvenNumberA(arr));
