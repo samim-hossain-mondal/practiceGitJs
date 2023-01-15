338a0b507a40ba640fb3031d2e99cabb65220b10
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-15 22:01:56 +0530

    feat: add functionality to find longest string in array

--- longestStringInArray.js
+++ longestStringInArray.js
@@ -1,11 +1,13 @@
 const readline = require("readline-sync");
 
+// User Input for array and arraySize
 let arrSize = Number(readline.question());
 let arr = [];
 for (let i = 0; i < arrSize; ++i) {
   arr.push(String(readline.question()));
 }
 
+// Normal Function
 function longestStrInArrayN(arr) {
   let maxStr = arr[0].length;
   let ans = arr[0];
@@ -18,7 +20,9 @@ function longestStrInArrayN(arr) {
   arr.forEach(findlongestStr);
   return ans;
 }
+console.log(longestStrInArrayN(arr));
 
+// Arrow Function
 let longestStrInArrayA = (arr) => {
   let maxStr = arr[0].length;
   let ans = arr[0];
@@ -31,6 +35,7 @@ let longestStrInArrayA = (arr) => {
   arr.forEach(findlongestStr);
   return ans;
 };
-
-console.log(longestStrInArrayN(arr));
 console.log(longestStrInArrayA(arr));
+
+// Exporting Functions
+module.exports = { longestStrInArrayN, longestStrInArrayA };
